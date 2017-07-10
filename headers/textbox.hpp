a82468dcb0010ca008293e0d952368f08fd7973f
Ákos Balogh <akos.balogh01@gmail.com>
2017-07-10 21:27:03 +0200

    textbox

--- headers/textbox.hpp
+++ headers/textbox.hpp
@@ -6,15 +6,22 @@ namespace chat {
     namespace graphics {
         class textbox: public interfaces::renderable {
             private:
+                unsigned    var_textbox_properties[6];
+                
                 unsigned    var_cursor_pos;
                 std::string var_cursor_front;
                 std::string var_cursor_back;
 
             public:
+                typedef enum {TOP = 0, BOTTOM = 1, LEFT = 2, RIGHT = 3} pos_type;
+                typedef enum {HEIGHT = 4, WIDTH = 5} size_type;
+
                 textbox();
 
+                unsigned position(textbox::pos_type);
+                unsigned size(textbox::size_type);
+
                 void render();
-                const std::string& text() const;
         }; 
     }
 }
--- sources/textbox.cpp
+++ sources/textbox.cpp
@@ -4,3 +4,14 @@
 void chat::graphics::textbox::render() {
     //do stuff
 }
+
+unsigned chat::graphics::textbox::position(textbox::pos_type type) {
+    switch (type) {
+        case textbox::TOP:    return var_textbox_properties[TOP];
+        case textbox::BOTTOM: return var_textbox_properties[BOTTOM];
+        case textbox::LEFT:   return var_textbox_properties[LEFT];
+        case textbox::RIGHT:  return var_textbox_properties[RIGHT];
+    }
+
+    return -1;
+}
